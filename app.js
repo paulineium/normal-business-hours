7cadcd2758b320278e4ec7af4e0f80c79002a8b9
paulineium <pk2196@nyu.edu>
2023-11-20 23:38:44 -0500

    Normal Business Hours

--- app.js
+++ app.js
@@ -1,48 +1,39 @@
+// app.js
 const express = require('express');
 const app = express();
 const port = 3000;
 
-// in middleware, the order matters...
-// check the hour first!
+// Middleware to check working hours
 app.use(workingHours);
-// serving our static files like always
+
+// Serving static files
 app.use(express.static('public'));
 
 app.listen(port, () => {
   console.log('Server listening at http://localhost:3000');
 });
 
-// our custom middleware function
+// Custom middleware function to check working hours
 function workingHours(req, res, next) {
-
-  // get the current time
   const currentTime = new Date();
-  // get the hour
   const currentHour = currentTime.getHours();
 
   const normalBusinessHours = {
-    // 24 hour time
-    open: 9,
-    close: 17,
+    open: 7,
+    close: 22,
   };
 
-  // check if within normal business hours
   if (
     currentHour >= normalBusinessHours.open &&
     currentHour <= normalBusinessHours.close
   ) {
-
-    // if so, point the request to our static files
-    console.log('Open!');
-    req.url = 'chauncey-gardiner-resume.pdf';
+    // If within normal business hours, proceed to static file serving
+    console.log('Open! 🔓');
     next();
-
   } else {
-    
-    // otherwise, return the denial
+    // If outside normal business hours, serve the denied.html file
     console.log('Closed 🔒');
-    req.url = 'denied.html';
+    req.url = '/denied.html';
     next();
-
   }
 }
